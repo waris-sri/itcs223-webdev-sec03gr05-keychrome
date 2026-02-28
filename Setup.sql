950a1058ee7e3d5131550cba33134a40ab7baf41
DRPITOS <123379947+DRPITOS@users.noreply.github.com>
2026-02-28 18:20:19 +0700

    Change data type.

--- Setup.sql
+++ Setup.sql
@@ -32,27 +32,27 @@ CREATE TABLE `Image`
 
 CREATE TABLE `Stocks`
 (
-    StockID VARCHAR(15)   NOT NULL,
+    StockID VARCHAR(15) NOT NULL,
     Color   VARCHAR(15) NOT NULL,
-    Amount  INT NOT NULL,
+    Amount  INT         NOT NULL,
     PRIMARY KEY (StockID)
 );
 
 CREATE TABLE `Product`
 (
-    SKU               VARCHAR(20)   NOT NULL,
-    Series            VARCHAR(15)   NOT NULL,
-    Description       VARCHAR(500)  NOT NULL,
-    Price             DECIMAL(7, 2) NOT NULL,
-    Type              ENUM ('Custom Keyboards', 'Keycaps', 'Mice', 'Slim Keyboards', 'Switches', 'Wired Keyboards', 'Wireless Keyboard', 'Z-Others')   NOT NULL,
+    SKU               VARCHAR(20)                                                                                                                    NOT NULL,
+    Series            VARCHAR(15)                                                                                                                    NOT NULL,
+    Description       VARCHAR(500)                                                                                                                   NOT NULL,
+    Price             DECIMAL(7, 2)                                                                                                                  NOT NULL,
+    Type              ENUM ('Custom Keyboards', 'Keycaps', 'Mice', 'Slim Keyboards', 'Switches', 'Wired Keyboards', 'Wireless Keyboard', 'Z-Others') NOT NULL,
     Sensor            VARCHAR(20),
     SwitchType        VARCHAR(20),
     Switch            VARCHAR(20),
     Version           VARCHAR(20),
     LayoutVersion     VARCHAR(20),
-    DiscountAvailable BOOLEAN       NOT NULL,
-    NewArrival        BOOLEAN       NOT NULL,
-    Rating            DECIMAL(2, 1) NOT NULL,
+    DiscountAvailable BOOLEAN                                                                                                                        NOT NULL,
+    NewArrival        BOOLEAN                                                                                                                        NOT NULL,
+    Rating            DECIMAL(2, 1)                                                                                                                  NOT NULL,
     ComboID           VARCHAR(15),
     ImageID           VARCHAR(15),
     StockID           VARCHAR(15),
