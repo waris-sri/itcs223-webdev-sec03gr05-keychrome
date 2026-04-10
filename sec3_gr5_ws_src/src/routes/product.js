ba652d3dce41b6cd1e0fac168ddb5894b35b1233
DRPITOS <123379947+DRPITOS@users.noreply.github.com>
2026-04-10 17:28:55 +0700

    Add dynamic `/api/product/:sku` route to fetch product details using SKU.

--- sec3_gr5_ws_src/src/routes/product.js
+++ sec3_gr5_ws_src/src/routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router();
 
-const { Client } = require('pg')
+const {Client} = require('pg')
 const dbClient = new Client(process.env.SUPABASE_CONNECTION_STRING)
 dbClient.connect()
 
@@ -15,5 +15,50 @@ router.get('/', async (req, res) => {
         res.status(500).send('Something broke!')
     }
 })
+
+// TODO: Add/Modify/Delete Product
+
+router.get('/:sku', async (req, res) => {
+    const productSku = req.params.sku;
+    console.log(`Req: /api/product/${productSku}`);
+
+    try {
+        const queryText = `
+			SELECT product.sku,
+			       product.series,
+			       product.description,
+			       product.price,
+			       product.type,
+			       product.sensor,
+			       product.switchtype,
+			       product.switch,
+			       version,
+			       layoutversion,
+			       discountavailable,
+			       newarrival,
+			       rating,
+			       comboid,
+			       stocks.amount AS stock_amount,
+			       image.source  AS image_source
+			FROM product
+					 JOIN stocks ON stocks.sku = product.sku
+				     JOIN image ON product.sku = image.sku
+			WHERE product.sku = $1;
+        `;
+        const queryValues = [productSku];
+
+        const result = await dbClient.query(queryText, queryValues);
+
+        if (result.rows.length === 0) {
+            return res.status(204).json({error: 'No Content'});
+        }
+
+        res.send(result.rows[0]);
+
+    } catch (err) {
+        console.error('Server error:', err);
+        res.status(500).send('Something broke!');
+    }
+});
 
 module.exports = router;
