e0e3400f21ee3deeca308cf37f7b70cb8ae97e5f
Waris Sripatoomrak <167295008+waris-sri@users.noreply.github.com>
2026-04-28 22:08:00 +0700

    Fix static file path syntax in Express setup

--- sec3_gr5_fe_src/src/fe.js
+++ sec3_gr5_fe_src/src/fe.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '../views'))
 
 // For Express
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(__dirname + "/public/"));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
